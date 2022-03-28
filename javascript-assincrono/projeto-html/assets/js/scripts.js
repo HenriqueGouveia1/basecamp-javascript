7ab8980de8f814825e4155b81821ccccd82a8264
henriquegouveia1 <henriquegs002@gmail.com>
2022-03-28 20:29:46 -0300

    Tentavida de replicar o aprendido na aula so async

--- javascript-assincrono/projeto-html/assets/js/scripts.js
+++ javascript-assincrono/projeto-html/assets/js/scripts.js
@@ -1,21 +1,23 @@
 const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const btn = document.getElementById('change-cat')
+
 
 const getCats = async () => {
-	try {
-		const data = await fetch(BASE_URL);
-		const json = await data.json();
-		return json.webpurl;
-	} catch (e) {
-		console.log(e.message);
-	}
-};
 
-const loadImg = async () => {
-	const img = document.getElementsByTagName('img')[0];
-	img.src = await getCats();
-};
+    try{
+        const data = await fetch(BASE_URL)
+        const json = await data.json()
+        return json.webpurl
+    }catch(e){
+        console.log(e.message)
+    }
+}
+
+const loadImg = async () =>{
+    const catImg = document.getElementById('cat')
+    catImg.src = await getCats();
+}
+
+btn.addEventListener('click', loadImg)
 
 loadImg();
-
-const btn = document.getElementById('change-cat');
-btn.addEventListener('click', loadImg);
